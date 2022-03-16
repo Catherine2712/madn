bf191804551492369d83237f58e2f4de1a4c6948
HKuehne <HKuehne@192.168.178.103>
2022-03-16 11:28:05 +0100

    Neueste Änderungen

--- src/madn/Regelwerk.java
+++ src/madn/Regelwerk.java
@@ -8,28 +8,55 @@ public class Regelwerk {
 	}
 	
 	
+	//out of range auffangen
+	private static boolean outOfRange(Spielfeld sf, Spielfigur auswahl, int wuerfel) {
+		int idxZiel;
+		if(arrayFinden(sf, auswahl)==1) {
+			idxZiel=sf.sucheFeldposition(sf.getFeld(), auswahl)+wuerfel;
+			if(idxZiel>=sf.getFeld().length) return true;
+			else return false;
+		}else return false;
+	}
+	
 	//Figur im Startfeld und 6 gewürfelt -->rauskommen erlaubt?
 	private static boolean rauskommenErlaubt(Spielfeld sf, Spielfigur auswahl, int wuerfel){
 		if((arrayFinden(sf, auswahl)==0 && wuerfel==6)) {
-			if(!(sf.getStartfeld()[0].getFarbe().equals(auswahl.getFarbe()))) {
+			if(!(sf.getFeld()[0].getFarbe().equals(auswahl.getFarbe()))) {
 				return true;
 			}else return false;	
 		}else return false;
 	}
 	
-	//Ziehen im Feld erlaubt?
+	//Ziehen im Hauptfeld erlaubt?
 	private static boolean ziehenErlaubt(Spielfeld sf, Spielfigur auswahl, int wuerfel) {
 		int idxZiel;
 		idxZiel=sf.sucheFeldposition(sf.getFeld(), auswahl)+wuerfel;
-		
 		if(arrayFinden(sf, auswahl)==1) {
+			if(outOfRange(sf, auswahl, wuerfel)) return true;												//Zug der aus dem Feld ins Zielfeld führt
 			if(!(auswahl.getFarbe().equals(sf.getFeld()[idxZiel].getFarbe()))) {
 				return true;
 			}else return false;
 		}else return false;
 	}
+		
+	//Ziel: Array ausgeben, in welchem die zu ziehende Figur liegt: Start: 0, Haupt: 1, Ziel:2
+	private static int arrayFinden(Spielfeld sf, Spielfigur auswahl) {
+		if(sf.sucheFeldposition(sf.getStartfeld(), auswahl) !=-1) {
+			return 0;
+		}else {
+			if(sf.sucheFeldposition(sf.getFeld(), auswahl)!=-1) {
+				return 1;
+			}else return 2;
+		}
+	}
 	
-	/*
+	/* //Ziehen ins Zielfeld
+	private static boolean ziehenInsZielfeldErlaubt(Spielfeld sf, Spielfigur auswahl, int wuerfel){
+		if(outOfRange(sf, auswahl, wuerfel)==true) return true;
+		return false;
+	}
+	
+	
 	//Index suchen und Rückgeben
 	private static int indexFinden(Spielfigur[] suchFeld, Spielfigur auswahl) {
 		int a=-1;
@@ -41,15 +68,4 @@ public class Regelwerk {
 		return a;
 	}
 	*/
-	
-	//Ziel: Array ausgeben, in welchem die zu ziehende Figur liegt: Start: 0, Haupt: 1, Ziel:2
-	private static int arrayFinden(Spielfeld sf, Spielfigur auswahl) {
-		if(sf.sucheFeldposition(sf.getStartfeld(), auswahl) !=-1) {
-			return 0;
-		}else {
-			if(sf.sucheFeldposition(sf.getFeld(), auswahl)!=-1) {
-				return 1;
-			}else return 2;
-		}
-	}
 }
