37af242601246735ea8b68952f858d000d8288b9
lehrer <lehrer@NBPC32.schule.local>
2022-03-16 10:04:13 +0100

    initialisieren der Spieler läuft noch nicht ganz

--- src/madn/Mensch_aergere_dich_nicht.java
+++ src/madn/Mensch_aergere_dich_nicht.java
@@ -1,14 +1,34 @@
 package madn;
+
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class Mensch_aergere_dich_nicht {
 	
-	//Attribute
-	int anzSpieler;
 	
+	// Anfang Attribute
+	//Attribute
+	private int anzSpieler;
+	private Spieler[] spieler;
+	//ArrayList<Spieler> meineSpieler = new ArrayList<Spieler>();
+	private Spielfeld meinSpielfeld;
+	private Regelwerk meinRegelwerk;
+	// Ende Attribute
+	
+	
+	//Konstruktor
+	public Mensch_aergere_dich_nicht (int anzSpieler, Spieler[] Spieler, Spielfeld meinSpielfeld, Regelwerk meinRegelwerk){
+		
+		this.anzSpieler = anzSpieler;  
+		//this.Spieler[] = Spieler[];
+		this.meinSpielfeld = meinSpielfeld;
+		this.meinRegelwerk = meinRegelwerk;
+	}
+		  
 	
 	//Methoden
-	public static int begruessen() {
+	public Spielfeld initialisiereSpielfeld() {
 		//Begruessung und Anzahl der Spieler waehlen
 		System.out.println("Herzlich Willkommen bei >Informatiker*in aergere dich nicht<!");
 		System.out.println("Bitte die Anzahl der Spieler*innen eingeben: ");
@@ -21,98 +41,59 @@ public class Mensch_aergere_dich_nicht {
 			System.out.println("Bitte die Anzahl der Spieler*innen eingeben: ");
 			anzSpieler = Integer.parseInt(sc.nextLine());
 		}
-		sc.close();
-	return anzSpieler;
-	}
-	
-	
-	public static void spielfeldVorbereiten(int anzSpieler) {
 		
-		//Scanner
-		Scanner sc = new Scanner(System.in);
-				
-		//Dummy Spieler
-		Spieler cathy = new Spieler("Cathy", "blau");
-		Spieler tobi = new Spieler("Tobi", "rot");
-		Spieler hendrik = new Spieler("Hendrik", "grün");
-		Spieler florian = new Spieler("Florian", "gelb");
-						
-		//aktives Spielfeld erzeugen
-		Spielfeld meinspielfeld=new Spielfeld(cathy, tobi, hendrik, florian);
-		//meinspielfeld.ausgabe();
-		//meinspielfeld.wuerfeln();			
-						
+		
 		//Spieler erstellen, Farben zuweisen, Spielfiguren erstellen und positionieren
 		//Dummy name
 		String name = "Max Muster";
 		for(int i = 0; i < anzSpieler; i++) {
-		System.out.println("Bitte den Namen der " + (i+1) + ". Person eingeben: ");
-		name = sc.nextLine();
 		
-		//Dummy farbe
-		String farbe = "bleu";
-							
-							
-		//Spieler 1, Spielfiguren setzen, Namen zuweisen
-		if(i == 0) {
-			farbe = "blau";
-			cathy.setName(name);
-			System.out.println("Hallo " + name + "! Deine Spielfiguren haben die Farbe " + farbe);
-			/*Spielfigur BlauA = new Spielfigur("blauA");
-			Spielfigur BlauB = new Spielfigur("blauB");
-			Spielfigur BlauC = new Spielfigur("blauC");
-			Spielfigur BlauD = new Spielfigur("blauD");*/
-							
-		}
-							
-						
-		//Spieler 2, Spielfiguren setzen, Namen zuweisen
-							
-		if(i == 1) {					
-			farbe = "gelb";				
-			tobi.setName(name);
-			System.out.println("Hallo " + name + "! Deine Spielfiguren haben die Farbe " + farbe);				
-			/*Spielfigur GelbA = new Spielfigur("gelbA");				
-			Spielfigur GelbB = new Spielfigur("gelbB");
-			Spielfigur GelbC = new Spielfigur("gelbC");
-			Spielfigur GelbD = new Spielfigur("gelbD");*/
-		}
-							
-		//Spieler 3, Spielfiguren setzen, Namen zuweisen
-		if(i == 2) {
-		farbe = "rot";
-		hendrik.setName(name);
-		System.out.println("Hallo " + name + "! Deine Spielfiguren haben die Farbe " + farbe);
-		/*Spielfigur RotA = new Spielfigur("gelbA");
-		Spielfigur RotB = new Spielfigur("gelbB");
-		Spielfigur RotC = new Spielfigur("gelbC");
-		Spielfigur RotD = new Spielfigur("gelbD");*/			
-		}
-							
-		//Spieler 4, Spielfiguren setzen, Namen zuweisen
-		if(i == 3) {
-			farbe = "gruen";
-			florian.setName(name);
-			System.out.println("Hallo " + name + "! Deine Spielfiguren haben die Farbe " + farbe);
-			/*Spielfigur GruenA = new Spielfigur("gruenA");
-			Spielfigur GruenB = new Spielfigur("gruenB");
-			Spielfigur GruenC = new Spielfigur("gruenC");
-			Spielfigur GruenD = new Spielfigur("gruenD");*/
-							
-		}
-							
-						
-		}
-						
-						
-						
+			Spieler[i]  = new Spieler;
+			
+			System.out.println("Bitte den Namen der " + (i+1) + ". Person eingeben: ");
+			name = sc.nextLine();
+			
+			//Dummy farbe
+			
+			String farbe = "bleu";
+			
+			switch(i) {
+			case 0: 
+				farbe = "blau";
+				//cathy.setName(name);
+				System.out.println("Hallo " + name + "! Deine Spielfiguren haben die Farbe " + farbe);
+				break;
+			case 1:
+				farbe = "gelb";				
+				//tobi.setName(name);
+				System.out.println("Hallo " + name + "! Deine Spielfiguren haben die Farbe " + farbe);				
+				break;
+			case 2:	
+				farbe = "rot";
+				//hendrik.setName(name);
+				System.out.println("Hallo " + name + "! Deine Spielfiguren haben die Farbe " + farbe);
+				break;
+			case 3:
+				farbe = "gruen";
+				//florian.setName(name);
+				System.out.println("Hallo " + name + "! Deine Spielfiguren haben die Farbe " + farbe);
+				break;	
+			}
+			
+			//aktives Spielfeld erzeugen
+			Spielfeld meinspielfeld=new Spielfeld(Spieler... spieler);
+			meinspielfeld.ausgabe();
+		
+		}			
 
-		
-		
+		//System.out.println(cathy.getName());
+		sc.close();
+	return meinSpielfeld;
 	}
 	
 	
-	public static void spielen() {
+
+	public void spielen() {
 		
 		
 		
@@ -126,8 +107,8 @@ public class Mensch_aergere_dich_nicht {
 		//Eigentlicher Spielablauf
 		while(finished != true) {
 			
-			for(int k = 0; k < 6; k++) {
-				
+			for(int k = 0; k < anzSpieler; k++) {
+				meinSpielfeld.wuerfeln();
 			}
 			
 			finished = true;
