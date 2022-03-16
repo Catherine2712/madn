2b8316e80b39ab8d0c79f41c75596e4cce09b415
lehrer <lehrer@NBPC32.schule.local>
2022-03-16 14:18:24 +0100

    kleinere Änderungen

--- src/madn/main.java
+++ src/madn/main.java
@@ -9,7 +9,7 @@ public class main {
 		
 		Mensch_aergere_dich_nicht game = new Mensch_aergere_dich_nicht();
 		game.initialisiereSpielfeld();
-		
+		game.spielen();
 		//MADN_ge.begruessen();
 		//MADN_ge.spielen(3);
 		
